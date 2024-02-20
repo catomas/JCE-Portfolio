25f652dd45f5bd9b76912588995f131b5250e3f0
Juan Camilo Echeverri Salazar <juancami98.11@hotmail.com>
2024-02-20 16:18:42 -0500

    home text

--- src/components/contact_me.tsx
+++ src/components/contact_me.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaInstagram } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io5";
 import { MdLocalPhone, MdOutlineEmail } from "react-icons/md";
+import { GrInstagram } from "react-icons/gr";
+import { LuInstagram } from "react-icons/lu";
 
 export const ContactMe = () => {
   return (
@@ -15,16 +20,32 @@ export const ContactMe = () => {
         repudiandae sunt eos ab odio quasi aperiam, accusantium laborum. Sed
         praesentium neque totam sit hic, doloremque in architecto laborum quia.
       </p>
-      <div className="flex ">
-        <MdOutlineEmail size="25" />
-        <h4 className="ml-2 font-workSans text-xl font-semibold">
-          losjuanes8@hotmail.com
-        </h4>
-      </div>
-      <div className="flex mt-2">
+
+      <Link href="tel:+573164156157" target="_blank" className="flex mt-2 ">
         <MdLocalPhone size="25" />
-        <h4 className="ml-2  text-xl font-semibold">+57 3164156157</h4>
-      </div>
+        <h4 className="ml-2  text-lg font-workSans">+57 3164156157</h4>
+      </Link>
+
+      <Link
+        href="mailto:juancarlos@juancarlosavaluos.com"
+        target="_blank"
+        className="flex mt-2"
+      >
+        <MdOutlineEmail size="25" />
+        <h4 className="ml-2 font-workSans text-lg   ">
+          juancarlos@juancarlosavaluos.com
+        </h4>
+      </Link>
+
+      <Link
+        href="https://www.instagram.com/juancarlosavaluos/"
+        target="_blank"
+        className="flex mt-2"
+      >
+        <LuInstagram size="25" />
+
+        <h4 className="ml-2  text-lg font-workSans">@juancarlosavaluos</h4>
+      </Link>
 
       <Image
         src="/icons/phone.svg"
--- src/components/home_page_component.tsx
+++ src/components/home_page_component.tsx
@@ -2,13 +2,14 @@
 
 import IsometricVideos from "@/components/isometric_videos";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export const HomePageComponent = () => {
   const useMotionProps = () => ({
     initial: { opacity: 0, scale: 0.5 },
     animate: { opacity: 1, scale: 1 },
     transition: {
-      duration: 2,
+      duration: 1,
       delay: 0.5,
       ease: [0, 0.71, 0.2, 1.01],
     },
@@ -18,17 +19,29 @@ export const HomePageComponent = () => {
     <>
       <motion.h1
         {...useMotionProps()}
-        className=" text-4xl sm:text-6xl md:text-7xl lg:text-8xl pb-6  sm:pb-10"
+        className=" text-4xl sm:text-6xl md:text-7xl  pb-6  sm:pb-10"
       >
         Juan Carlos Echeverri Avalúos
       </motion.h1>
       <div className="flex flex-col lg:flex-row">
-        <motion.p {...useMotionProps()} className="pb-6  sm:text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio eos
-          deserunt beatae eaque officia eveniet ratione animi, necessitatibus
-          dolor mollitia magni cum! Omnis labore neque blanditiis repudiandae,
-          placeat earum sed.
-        </motion.p>
+        <motion.div {...useMotionProps()} className="pb-6 italic  sm:text-xl">
+          <p>
+            "Soy un experimentado avaluador certificado por la Lonja de
+            Avaluadores, con una sólida trayectoria en el campo de la valoración
+            de bienes. Con años de experiencia en la industria, estoy
+            comprometido a proporcionar evaluaciones precisas y confiables para
+            una variedad de propósitos"
+          </p>
+          <div className="flex  items-end justify-end">
+            <Image
+              className="mt-5"
+              src="/images/logo-longa.png"
+              width={50}
+              height={50}
+              alt="logo lonja"
+            />
+          </div>
+        </motion.div>
 
         <IsometricVideos />
       </div>
