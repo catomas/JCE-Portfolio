ebfccbdfc93b9339f0517f0a0d16cb688866e794
Juan Camilo Echeverri Salazar <juancami98.11@hotmail.com>
2024-02-20 16:30:40 -0500

    fix text home

--- src/components/contact_me.tsx
+++ src/components/contact_me.tsx
@@ -12,13 +12,14 @@ export const ContactMe = () => {
       <h1 className=" font-vollkorn text-4xl md:text-6xl md:mx-auto">
         Contactame
       </h1>
-      <h2 className=" mt-4 text-xl md:text-2xl font-semibold">
+      <h2 className="  font-vollkorn mt-4 text-xl md:text-2xl font-semibold">
         Trabajemos juntos
       </h2>
-      <p className=" mt-4 mb-9">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam qui
-        repudiandae sunt eos ab odio quasi aperiam, accusantium laborum. Sed
-        praesentium neque totam sit hic, doloremque in architecto laborum quia.
+      <p className="  text-lg mt-4 mb-9">
+        Si estás interesado en mis servicios de evaluación o tienes alguna
+        pregunta, no dudes en ponerte en contacto conmigo. Estoy aquí para
+        ayudarte a encontrar soluciones adecuadas para tus necesidades de
+        valoración.
       </p>
 
       <Link href="tel:+573164156157" target="_blank" className="flex mt-2 ">
